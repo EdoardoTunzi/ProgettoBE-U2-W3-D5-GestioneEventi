aad77cb60b47126fdc62643d6bd8b23f227ebf01
Edoardo <edfl.tunzi@gmail.com>
2025-02-21 14:56:31 +0100

    initial commit

---
+++ src/main/java/com/example/ProgettoBE_U2_W3_D5_GestioneEventi/ProgettoBeU2W3D5GestioneEventiApplication.java
@@ -0,0 +1,13 @@
+package com.example.ProgettoBE_U2_W3_D5_GestioneEventi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgettoBeU2W3D5GestioneEventiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgettoBeU2W3D5GestioneEventiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ProgettoBE_U2_W3_D5_GestioneEventi/ProgettoBeU2W3D5GestioneEventiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ProgettoBE_U2_W3_D5_GestioneEventi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgettoBeU2W3D5GestioneEventiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
